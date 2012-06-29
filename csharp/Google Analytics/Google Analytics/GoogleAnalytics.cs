9eedbf6386bd038d8989795abe82af769b765622
Rafe Kemmis <rafe.kemmis@mckinney.com>
2012-06-29 14:37:09 -0400

    Added C# Google Analytics server side tracking script

---
+++ csharp/Google Analytics/Google Analytics/GoogleAnalytics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace McKinney.Analytics
+{
+    public class GoogleAnalytics
+    {
+        private static List<GAEvent> EventQueue { 
+            get
+            {
+                
+                if(HttpContext.Current.Session["gaq"] == null)
+                {
+                    HttpContext.Current.Session["gaq"] = new List<GAEvent>();
+                }
+                return HttpContext.Current.Session["gaq"] as List<GAEvent>;
+            }
+            
+        }
+
+        public static void PushEvent(string category, string action, string label = null)
+        {
+            EventQueue.Add(new GAEvent
+                               {
+                                   Category = category,
+                                   Action = action,
+                                   Label = label
+                               });
+        }
+
+        public static IHtmlString OutputEvents()
+        {
+            StringBuilder s = new StringBuilder();
+            for(int i = EventQueue.Count-1; i>= 0; i--)
+            {
+                GAEvent ev = EventQueue[i];
+                s.AppendLine(ev.ToString());
+                EventQueue.RemoveAt(i);
+            }
+            return new HtmlString(s.ToString());
+        }
+    }
+
+    public class GAEvent
+    {
+        public string Category { get; set; }
+        public string Action { get; set; }
+        public string Label { get; set; }
+
+        private  string template = "_gaq.push(['_trackEvent', '{0}', '{1}', '{2}']);";
+
+        public override string ToString()
+        {
+            return string.Format(template, Category, Action, Label);
+        }
+    }
+}
---
+++ csharp/Google Analytics/Google Analytics/README.md
@@ -0,0 +1,7 @@
+﻿*** Google Analytics - from the server side!
+
+**** Queue up events anywhere in your asp.net code like this:
+GoogleAnalytics.PushEvent("Login","Facebook");
+
+**** Then in your view you ouput calls like so:
+@GoogleAnalytics.OutputEvents()
