e656a43f9f229d0908dcacf290971d0ed4079199
Rafe Kemmis <rafe.kemmis@mckinney.com>
2012-06-29 16:01:47 -0400

    added example mvc 3 website that uses Google Analytics script

---
+++ csharp/Google Analytics/Example/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using McKinney.Analytics;
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            // something special happened, let's track it
+            GoogleAnalytics.PushEvent("My Category", "My Action", "An optional label");
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
